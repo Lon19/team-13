5016f21dbdf0b62861a7294ca553cb6626a81349
George <gemiti@uahoo.com>
2019-11-02 02:33:36 +0000

    search implemented

--- MovementMap/MovementApp/fetching.py
+++ MovementMap/MovementApp/fetching.py
@@ -1,5 +1,22 @@
 import requests
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+from geopy.geocoders import Nominatim
+
 
 def fetch_all():
-  url = 'http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.jsonstat.json?date=latest&Gender=0,1,2&Age=11,12'
-  return requests.get(url).json()
+    url = 'http://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.jsonstat.json?date=latest&Gender=0,1,2&Age=11,12'
+    return requests.get(url).json()
+
+
+def gel_location(address):
+    geolocator = Nominatim('Looking for address...')
+    location = geolocator.geocode(address)
+    lat, lon = location.latitude, location.longitude
+    return Point(lat, lon)
+
+
+def is_in_polygon(point, points):
+    return Polygon(points).contains(point)
+
+
