57a0728c18452e2bf6e5b3d22d54f65ef36e523b
charleschentianxiao <ctx5275@gmail.com>
2019-11-02 00:31:50 +0000

    updated view.py

--- MovementMap/MovementApp/views.py
+++ MovementMap/MovementApp/views.py
@@ -1,8 +1,28 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+import json
 
 
 def index(request):
     return render(request, 'MovementApp/index.html')
+
+
+def submit(request):
+    with open('/Users/admin/Desktop/wards.json') as f:
+      txt = f.read()
+    text = json.loads(txt)
+    wards = text["features"]
+    l = []
+    for dic in wards:
+        new_dict = {"Name": dic["properties"]["wd16nm"], "ID": dic["properties"]["wd16cd"],
+                    "Coordinates": dic["geometry"]["coordinates"][0][0]}
+        l.append(new_dict)
+
+    result = {'rubbish data': l}
+
+    return HttpResponseRedirect('/', context=result)
+
+
+
